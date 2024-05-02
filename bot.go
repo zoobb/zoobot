f8e752be2d5415457e12cb2c19e292a17c114ccf
zoob <zubovalexey2001@gmail.com>
2024-05-02 21:45:36 +0300

    Testing update method

--- bot.go
+++ bot.go
@@ -1,1 +1,46 @@
-package zoobot
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ZubovSL/zoobot/types"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	//fmt.Println("Hello World")
+
+	// https://api.telegram.org/bot<token>/METHOD_NAME
+	botURL := "https://api.telegram.org/bot" + os.Getenv("BOT_TOKEN")
+	fmt.Println(getUpdates(botURL))
+}
+
+func getUpdates(botURL string) ([]types.Update, error) {
+	resp, err := http.Get(botURL + "/getUpdates")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response types.Response
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, err
+	}
+	return response.Result, nil
+}
--- types/types.go
+++ types/types.go
@@ -1,1 +1,14 @@
 package types
+
+type Update struct {
+	UpdateID int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+type Message struct {
+	MessageID int `json:"message_id"`
+}
+
+type Response struct {
+	Result []Update `json:"result"`
+}
